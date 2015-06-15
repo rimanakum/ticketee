4b9f66dcebe412fd42bf63cc050bf00ce3919063
Rima Nakum <rima.nakum@aspiresoftware.in>
2015-06-15 11:55:24 +0530

    Restrict destroy action to only people with permission

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -9,6 +9,7 @@ feature 'Deleting tickets' do
 	end
 	before do
 		define_permission!(user, "view", project)
+		define_permission!(user, "delete tickets", project)
 		sign_in_as!(user)
 		visit '/'
 		click_link project.name
